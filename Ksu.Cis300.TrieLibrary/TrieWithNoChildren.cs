9a73498b92fd35670644fa5f8a16039b0761fdbd
chaliha <chaliha@CS-CLINTON>
2019-03-25 15:20:25 -0500

    Attempted

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithNoChildren : ITrie
+    {
+        private bool _emptystring = false;
+
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _emptystring = true;
+            }
+            if(s == "" || s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+
+        }
+        public bool Contains(string s)
+        {
+            if(s == "") { return _emptystring; }
+            return false;
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChildren.cs
@@ -0,0 +1,52 @@
+﻿///Author: Shreyas Chaliha
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithOneChild : ITrie
+    {
+        private bool _emptystr;
+        private ITrie _onlychild;
+        private char _childlabel;
+
+        public TrieWithOneChild(string str, bool emptystr)
+        {
+            if(str == "" || str[0] < 'a' || str[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            _emptystr = emptystr;
+        }
+
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _emptystr = true;
+                return this;
+            }
+            if(s[0] == _childlabel) { Add() }
+
+            if(s[0] != _childlabel) { }
+
+
+        }
+
+        public bool Contains(string str)
+        {
+            if (str == "") 
+            {
+                return _emptystr;
+            }
+            else if(str[0] == _childlabel)
+            {
+                return _onlychild.Contains(str.Substring(0, 1);
+            }
+            return false;
+        }
+    }
+}
